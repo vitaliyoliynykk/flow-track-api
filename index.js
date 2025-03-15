fe08702f0df783d3259c456e4dd4b9946c41ee24
Vitalii <vitaliyoliynykk@gmail.com>
2025-03-15 16:54:13 +0200

    model & nodemon

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const User = require("./models/User");
 require("dotenv").config();
 
 const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
 });
 
 const PORT = process.env.PORT || 5000;
