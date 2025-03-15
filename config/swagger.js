be16edfb30f7f91a47876ae8f699817d8645ebc0
Vitalii <vitaliyoliynykk@gmail.com>
2025-03-15 17:59:55 +0200

    swagger init

---
+++ config/swagger.js
@@ -0,0 +1,27 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "FlowTrack API",
+      version: "1.0.0",
+      description: "Документація API для FlowTrack",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+const setupSwagger = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const setupSwagger = require("./config/swagger");
 
 const authRoutes = require("./routes/authRoutes");
 
@@ -8,6 +9,7 @@ require("dotenv").config();
 const app = express();
 
 connectDB();
+setupSwagger(app);
 
 app.use(express.json());
 app.use("/auth", authRoutes);
