c8b7cf7b5b0501c339db3b572abd87b58fff40f5
Nataliia Leshchak <n.leshchak@levi9.com>
2025-03-22 11:22:22 +0200

    fix: fix CR comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,25 +4,21 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-router.post(
-  '/:userId/update-user-profile-picture',
-  async ({ body, params }, res) => {
-    const { userId } = params;
-    const { profilePicture } = body;
+router.post('/update-user-profile-picture-url', async ({ user, body }, res) => {
+  const { profilePictureUrl } = body;
 
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: userId },
-        { profile_picture_url: profilePicture },
-        { new: true }
-      );
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user.id },
+      { profile_picture_url: profilePictureUrl },
+      { new: true }
+    );
 
-      res.status(200).json(updatedUser);
-    } catch (e) {
-      console.error('Failed to update user profile picture', e);
-      res.status(500).json({ error: 'Server error' });
-    }
+    res.status(200).json(updatedUser);
+  } catch (e) {
+    console.error('Failed to update user profile picture', e);
+    res.status(500).json({ error: 'Server error' });
   }
-);
+});
 
 module.exports = router;
