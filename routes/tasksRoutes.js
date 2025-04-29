2b243567e5d84cc9028f7cef0d2ad0eb677dfd2b
Vitalii <vitaliyoliynykk@gmail.com>
2025-04-29 12:15:12 +0300

    fix

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -21,11 +21,26 @@ router.put("/", async ({ user, body }, res) => {
       user_id: user.id,
       title: body.title,
       status: body.status,
-      sessions_goal: body.sessionsGoal,
+      sessions_goal: body.sessions_goal,
       end_date: body.endDate,
     };
 
     const result = await Task.insertOne(task);
+
+    res.status(200).json(result);
+  } catch (e) {
+    console.error("Failed to create a task", e);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.patch("/update-completed", async ({ user, body }, res) => {
+  try {
+    const result = await Task.findByIdAndUpdate(
+      body.task_id,
+      { sessions_completed: body.sessions_completed },
+      { new: true }
+    );
 
     res.status(200).json(result);
   } catch (e) {
